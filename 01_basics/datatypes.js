0755b986ff2a4d86cc04569ed898068fdcb85c4d
kundan069 <134853977+kundan069@users.noreply.github.com>
2025-10-07 13:35:55 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"
+console.log(3+4);
+
+// alert(3+3) // ERROR : we are using nodejs, not browser
+let name = "Kundan"
+let age = 22
+let adult = true
+let state = null
+
+//datatypes
+// number => 2^53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness find krne k liye 
+
+// object
+
+console.log(typeof null);
