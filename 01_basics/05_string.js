3016ad1139d8aa271b3bdf4fd3701c99643aa82c
kundan069 <134853977+kundan069@users.noreply.github.com>
2025-10-08 19:40:32 +0000

    string and its methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+// const name = "Kundan"
+// const repoCount = 3
+// console.log(`Hello My name is ${name} and my repo Count is ${repoCount}`);
+
+// // Another way of intialising string 
+
+// const gameName = new  String('Kundan');
+// console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toLowerCase());
+
+//slice ,trim,replace,substring,indexof,charAt
+
+
