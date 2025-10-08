44282026421813e4e0b715ead32d6d38c69eea94
kundan069 <134853977+kundan069@users.noreply.github.com>
2025-10-08 12:21:35 +0000

    comaprision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+console.log(null == 0);
+console.log(null != 0);
+
+console.log("**** Undefined Comparision ****");
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+console.log(undefined <= 0);
+console.log(undefined == 0);
+console.log(undefined != 0);
+
+// >,<,>=,<= works differently (conversion done differently)
+// === strict check datatypes also checked
