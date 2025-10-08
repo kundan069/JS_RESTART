d4d22ecd7e775e440a28b72a3c3e1898b376b03d
kundan069 <134853977+kundan069@users.noreply.github.com>
2025-10-08 19:20:28 +0000

    stack and heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,7 +1,7 @@
 // Primitive
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
-
+/*
 const score = 100
 const scorevalue = 101.9
 const isLogged = false
@@ -31,5 +31,27 @@ let myObj = {
 const myFunc = function(){
     console.log("Hello! Mr. Kundan");    
 }
-
+*/
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive)  => create copy 
+// Heap (Non-Primitive)  =>  Reference milta original value ka 
+
+ 
+let myName = "Kundan"
+let yourName = "Sandeep"
+yourName = "Piyush"
+console.log(yourName);
+console.log(myName);
+
+let userOne = {
+    email : "alpha@gmail.com",
+    upi : "userOne@ybl"
+};
+let userTwo = userOne ;
+userTwo.email = "beta@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
