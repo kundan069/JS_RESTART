8e17b2cef1fb879214e3806c76b3e90d2b583a7c
kundan069 <134853977+kundan069@users.noreply.github.com>
2025-10-12 20:40:54 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+// *************** Control Flow *****************
+
+// ---- if ----
+const isUserLoggedIn = true;
+if(isUserLoggedIn){
+    console.log("User is Logged IN !");
+}
+else{
+    console.log("User Logged Out");
+}
+
+// comparision operator :  < , > , <= , >= , == , != , ===
+/*  if(condition){
+    } else {
+    }
+ */
+
+// -------- scope ---------
+// let score = 200;
+// if(score > 100){
+//     let power = "fly";
+//     console.log(`user power : ${power}`);
+// }
+// console.log(`user power : ${power}`);
+
+
+// ---------- short hand noation ---------  
+// const balance = 1000
+// if(balance > 500) console.log("test1"),   // implicit scope
+// console.log("test2");
+
+// ----------- nested if-else -------------
+// const num1 = 500 
+// if(num1>300){
+//     if(num1 > 1000){
+//         console.log(`${num1} is greater than  1000 `);
+//     }else{
+//         console.log(`${num1} is greater than 300 but less than 1000`);
+//     }
+// }else{
+//     console.log(`${num1} is less than 300`);
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course ! ");
+}
