192f8ba505c87626ec5b5007a8cec0bd5e9e0ea8
kundan069 <134853977+kundan069@users.noreply.github.com>
2025-10-14 21:31:53 +0000

    reduce , filter

--- 05_iterations/07_seven.js
+++ 05_iterations/07_seven.js
@@ -5,4 +5,17 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 //chaining function
 const newNumber = myNumbers.map( (num)=> num*10 ).map( (num)=> num+1).filter( (item)=> item >=42)
 
-console.log(newNumber);
+//console.log(newNumber);
+
+
+// reduce method 
+
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc,currval){
+//     return acc+ currval
+// },0)
+
+
+const myTotal = myNums.reduce((acc,curr) => acc+curr , 0)
+console.log(myTotal);
